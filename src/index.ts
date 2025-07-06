6e188425ee0034cfdb7c6f8317209cdbf944e18a
ratneshster <36915507+ratneshster@users.noreply.github.com>
2025-07-06 12:32:41 +1000

    Update index.ts
    Adding Laughing Budhha -T1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "Laughing Budhha",
     };
 
     const response = await env.AI.run(
